8fb82522918f7eaf158f9ad273aa0efef0370c74
didiraja <didiraja@hotmail.com>
2025-10-26 11:57:52 -0300

    feat: 🎸 Show selected Bairro

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,32 @@
 "use client";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import { GeoJSON } from "react-leaflet/GeoJSON";
 import bairros from "../libs/bairros.json";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
-export default function Map() {
+export default function Map({ onSelectBairro }) {
   return (
     <MapContainer
-      className="w-[500px] h-[500px] border-2 border-blue-600"
-      center={[-22.908333, -43.196388]}
-      zoom={14}
+      className="w-full h-[80%]"
+      center={[-22.89642, -43.47223]}
+      zoom={11}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeoJSON data={bairros.features} />
-      <Marker position={[-43.336323599767375, -22.825616607053909]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <GeoJSON
+        data={bairros.features}
+        eventHandlers={{
+          click: (e) => {
+            onSelectBairro(e.layer.feature.properties);
+          },
+        }}
+      />
     </MapContainer>
   );
 }
