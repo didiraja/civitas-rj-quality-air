b1638ce148fd9c39e5881f2a28d6771e981bc665
didiraja <didiraja@hotmail.com>
2025-10-26 13:07:22 -0300

    refactor: 💡 fix TS; change data source from deprecated key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import Map from "../components/Map";
 
 export default function Home() {
-  const [selectedBairro, setSelectedBairro] = useState(null);
+  const [selectedBairro, setSelectedBairro] = useState<{ nome: string } | null>(
+    null
+  );
 
   return (
     <main className="p-8 h-screen">
-      {/* <Map onSelectBairro={setSelectedBairro} /> */}
+      <Map onSelectBairro={setSelectedBairro} />
       {selectedBairro && (
         <p className="mt-4 font-bold text-xl">{selectedBairro.nome}</p>
       )}
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,12 +2,22 @@
 
 import { MapContainer, TileLayer } from "react-leaflet";
 import { GeoJSON } from "react-leaflet/GeoJSON";
+import { FeatureCollection, MultiPolygon } from "geojson";
 import bairros from "../libs/bairros.json";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
-export default function Map({ onSelectBairro }) {
+export default function Map({
+  onSelectBairro,
+}: {
+  onSelectBairro: ({ nome }: { nome: string }) => void;
+}) {
+  const typedBairros = bairros as FeatureCollection<
+    MultiPolygon,
+    { nome: string }
+  >;
+
   return (
     <MapContainer
       className="w-full h-[80%]"
@@ -20,10 +30,10 @@ export default function Map({ onSelectBairro }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <GeoJSON
-        data={bairros.features}
+        data={typedBairros}
         eventHandlers={{
           click: (e) => {
-            onSelectBairro(e.layer.feature.properties);
+            onSelectBairro(e.sourceTarget.feature.properties);
           },
         }}
       />
