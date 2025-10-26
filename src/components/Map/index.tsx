a03d069e4622a430281d7c40471d64013b722568
didiraja <didiraja@hotmail.com>
2025-10-26 16:36:31 -0300

    feat: 🎸 Install Tanstack Query

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,12 +2,12 @@
 
 import { MapContainer, TileLayer } from "react-leaflet";
 import { GeoJSON } from "react-leaflet/GeoJSON";
-import bairros from "../libs/bairros.json";
+import bairros from "../../libs/bairros.json";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import { LeafletMouseEvent } from "leaflet";
-import { IFeatureBairro } from "../types";
+import { IFeatureBairro } from "../../types";
 
 type Props = {
   onClickBairro: (e: LeafletMouseEvent) => void;
