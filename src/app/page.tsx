8d04540a3487eb0ddd9186149b420a7b3844ca25
didiraja <didiraja@hotmail.com>
2025-10-28 20:27:44 -0300

    style: 💄 global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import dynamic from "next/dynamic";
-import { center, centerOfMass } from "@turf/turf";
+import { centerOfMass } from "@turf/turf";
 import { useAirPollution } from "../libs/services/weather/getAirPollution";
 import { IFeatureBairro } from "../types";
 
@@ -44,53 +44,65 @@ export default function Home() {
 
   return (
     <main className="p-8 h-screen">
-      <DynamicMap onClickBairro={setSelectedBairro} />
-      {selectedBairro && (
-        <>
-          <p className="mt-4 font-bold text-xl">
-            {selectedBairro.properties.nome}
-          </p>
-          {qualityAirData && (
+      <h1 className="text-3xl font-bold text-center mb-8 text-white">
+        Painel de Qualidade do Ar do Rio de Janeiro
+      </h1>
+
+      <div className="h-[80%] grid grid-cols-[.7fr_.3fr] gap-8">
+        <DynamicMap onClickBairro={setSelectedBairro} />
+
+        <div className="air-data">
+          {selectedBairro && (
             <>
-              <p className="text-sm">
-                <strong>Qualidade:</strong> {selectedAirData?.aqi}
+              <p className="font-bold text-3xl mb-4 text-white">
+                {selectedBairro.properties.nome}
               </p>
-              <p className="text-sm">
-                <strong>Categoria:</strong> {selectedAirData?.category}
-              </p>
-              {selectedPollutants && (
+              {qualityAirData && (
                 <>
-                  <p className="text-sm">
-                    <strong>Poluentes:</strong>
+                  <p className="detail-text">
+                    <strong>Qualidade:</strong> {selectedAirData?.aqi}
                   </p>
-                  {selectedPollutants.map(
-                    ({ code, fullName, displayName, concentration }) => {
-                      return (
-                        <p className="text-sm" key={code}>
-                          - <span className="font-semibold">{displayName}</span>{" "}
-                          {concentration.value} {concentration.units}
-                        </p>
-                      );
-                    }
+                  <p className="detail-text">
+                    <strong>Categoria:</strong> {selectedAirData?.category}
+                  </p>
+                  {selectedPollutants && (
+                    <>
+                      <p className="detail-text">
+                        <strong>Poluentes:</strong>
+                      </p>
+                      {selectedPollutants.map(
+                        ({ code, displayName, concentration }) => {
+                          return (
+                            <p className="detail-text" key={code}>
+                              -{" "}
+                              <span className="font-semibold">
+                                {displayName}
+                              </span>{" "}
+                              {concentration.value} {concentration.units}
+                            </p>
+                          );
+                        }
+                      )}
+                    </>
                   )}
-                </>
-              )}
-              {selectedRecommendations && (
-                <>
-                  <p className="text-sm mt-2">
-                    <strong>Recomendações de saúde:</strong>
-                  </p>
-                  {selectedRecommendations.map((rec, index) => (
-                    <p className="text-sm" key={index}>
-                      - {rec}
-                    </p>
-                  ))}
+                  {selectedRecommendations && (
+                    <>
+                      <p className="detail-text">
+                        <strong>Recomendações de saúde:</strong>
+                      </p>
+                      {selectedRecommendations.map((rec, index) => (
+                        <p className="detail-text" key={index}>
+                          - {rec}
+                        </p>
+                      ))}
+                    </>
+                  )}
                 </>
               )}
             </>
           )}
-        </>
-      )}
+        </div>
+      </div>
     </main>
   );
 }
